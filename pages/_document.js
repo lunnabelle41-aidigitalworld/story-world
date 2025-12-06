18392061aba7741d98c4ed00c26cd26c6a87cecb
AI-Vault-tech <your-email@example.com>
2025-12-06 08:08:16 +0500

    Add Google Search Console verification meta tag to _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="google-site-verification" content="_xAnSYTjZ9yWZeJ4e2YXlHtZ7Of6p3A5TFCMbgDeO-I" />
+      </Head>
       <body>
         <Main />
         <NextScript />
