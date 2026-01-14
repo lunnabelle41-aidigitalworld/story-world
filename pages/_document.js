e9b85b81958ebada4ebce02e3652354fdcf48f8e
Your Name <you@example.com>
2026-01-14 20:03:06 +0500

    Add monetag verification code to head section

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="google-site-verification" content="f08c47fec0942fa0" />
+        <meta name="monetag" content="6d7093fd1eee3952080b256cacd1f049" />
         <script 
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2894915343289598" 
